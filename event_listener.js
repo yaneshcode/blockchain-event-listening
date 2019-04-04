f10bb0479ad900c5cad3cb1753e328730aca0f23
yanesh <yaneshcode@gmail.com>
2019-04-03 20:09:56 -0400

    event listener

---
+++ event_listener.js
@@ -0,0 +1,276 @@
+const ethers = require('ethers');
+const credentials = require('./credentials/credentials.js');
+
+let provider = new ethers.providers.InfuraProvider("kovan", credentials.INFURA_API_KEY);
+
+let wallet = new ethers.Wallet(credentials.P_KEY, provider);
+
+// ERC20 interface
+let abi = [
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "name",
+        "outputs": [
+            {
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_spender",
+                "type": "address"
+            },
+            {
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "approve",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "totalSupply",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_from",
+                "type": "address"
+            },
+            {
+                "name": "_to",
+                "type": "address"
+            },
+            {
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "transferFrom",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "decimals",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint8"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "_owner",
+                "type": "address"
+            }
+        ],
+        "name": "balanceOf",
+        "outputs": [
+            {
+                "name": "balance",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "symbol",
+        "outputs": [
+            {
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_to",
+                "type": "address"
+            },
+            {
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "transfer",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "_owner",
+                "type": "address"
+            },
+            {
+                "name": "_spender",
+                "type": "address"
+            }
+        ],
+        "name": "allowance",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "payable": true,
+        "stateMutability": "payable",
+        "type": "fallback"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "name": "owner",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "name": "spender",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "name": "value",
+                "type": "uint256"
+            }
+        ],
+        "name": "Approval",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "name": "from",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "name": "to",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "name": "value",
+                "type": "uint256"
+            }
+        ],
+        "name": "Transfer",
+        "type": "event"
+    }
+];
+
+
+// 0x address on Kovan
+let contractAddress = "0x2002D3812F58e35F0EA1fFbf80A75a38c32175fA";
+
+let contract = new ethers.Contract(contractAddress, abi, provider);
+
+console.log("Filtering address: " + wallet.address);
+console.log("\n");
+
+(async function() {
+  let transferEvent = new Promise((resolve, reject) => {
+    contract.on('Transfer', (from, to, amount, event) => {
+      console.log("From  : " + from);
+      console.log("To    : " + to);
+      console.log("Amount: " + amount);
+      console.log("\n");
+
+      resolve({
+        from: from,
+        to: to,
+        amount: amount
+      });
+    });
+
+    // filtering addresses on event
+    let filterFromMe = contract.filters.Transfer(wallet.address, null);
+    let filterToMe = contract.filters.Transfer(null, wallet.address);
+    let handleEvent = function(from, to) {
+      console.log("Filtered From  : " + from);
+      console.log("Filtered To    : " + to);
+      console.log("\n");
+    };
+    contract.on(filterFromMe, handleEvent);
+    contract.on(filterToMe, handleEvent);
+
+  });
+
+  let event = await transferEvent;
+  //
+  // console.log(event);
+  // console.log("From: " + event.from);
+  // console.log("To: " + event.to);
+  // console.log("Amount: " + event.amount);
+
+})();
